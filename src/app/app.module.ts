c227c2753e515f68f9a6a29ea620acbac798c8c4
bettoarcaya <betoarcaya@gmail.com>
2017-12-29 11:02:53 -0400

    importando modulos faltantes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
+/**modulos */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
 
+/**components */
 import { AppComponent } from './app.component';
 
+/**librerias */
 import { AgmCoreModule } from '@agm/core';
 
 
@@ -13,6 +18,8 @@ import { AgmCoreModule } from '@agm/core';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCGuqEpKPHOWXc99M0Va4fH4WwcCWm1T5s'
     })
