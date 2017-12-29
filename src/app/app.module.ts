899e7e0d645e1c81f621697e6c383b9c11be3350
bettoarcaya <betoarcaya@gmail.com>
2017-12-29 08:48:49 -0400

    instalando libreria de google maps en el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCGuqEpKPHOWXc99M0Va4fH4WwcCWm1T5s'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
