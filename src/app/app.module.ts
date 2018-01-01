4356e50fe48303361e7f49c9ec0d13f3ed22fa30
bettoarcaya <betoarcaya@gmail.com>
2018-01-01 14:43:02 -0400

    implementando rutas en el proyecto / creando vista para el home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
+import {Routes, RouterModule} from "@angular/router";
 
 
 /**components */
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 /**librerias */
 import { AgmCoreModule } from '@agm/core';
 
+const appRoutes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'Home', component: HomeComponent},
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes),
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCGuqEpKPHOWXc99M0Va4fH4WwcCWm1T5s'
     })
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
